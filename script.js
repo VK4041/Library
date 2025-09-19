a1ed92879cbdcfd5233c7bdd594582add170398d
VK4041 <151887513+VK4041@users.noreply.github.com>
2025-09-19 12:40:49 +1000

    Add basic styling and grid layout

---
+++ script.js
@@ -0,0 +1,14 @@
+const myLibrary = []
+
+const Book = function (id, name, author, color) {
+    this.id = id;
+    this.name = name;
+    this.author = author;
+    this.color = color
+}
+
+const addBooktoLibrary = function (name, author, color) {
+    let id = crypto.randomUUID();
+    let bookObj = new Book(id, name, author, color);
+    myLibrary.push(bookObj)
+}
